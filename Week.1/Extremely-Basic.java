ffedf4105c7ffc40ee7a127ec45677cafec09ce6
EslamAborya <eslam.aborya1251@gmail.com>
2023-04-06 06:59:35 +0200

    Extremely Basic problem solved

---
+++ Week.1/Extremely-Basic.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner in =new Scanner(System.in);
+
+        int a=in.nextInt();
+        int b=in.nextInt();
+        int x=a+b;
+        System.out.println("X = "+x);
+
+    }
+
+}
