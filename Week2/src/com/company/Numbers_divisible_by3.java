c7bbd83619943179b6036fd65e5123fb88155aa0
EslamAborya <eslam.aborya1251@gmail.com>
2023-04-12 20:36:58 +0200

    week2 tasks has been finished

---
+++ Week2/src/com/company/Numbers_divisible_by3.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.*;
+public class Numbers_divisible_by3 {
+    public static void main(String[] args){
+        for(int i=1; i<100;i++)
+        {
+            if(i%3==0)
+            {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
---
+++ Week2/src/com/company/check.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.*;
+public class check {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int num;
+        num=in.nextInt();
+
+        if(num%2==0)
+        {
+            System.out.println("Even number");
+        }
+        else
+            System.out.println("Odd number");
+    }
+}
